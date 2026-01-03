cb611d9ccd2f30880e84ae2de1893fddb8a5e149
Daniel Menezes <daniel.menezes.md@gmail.com>
2026-01-03 19:56:25 -0300

    feature: persistir dados no local storage

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map } from 'rxjs';
+import { BehaviorSubject, map, tap } from 'rxjs';
 import { ITask } from '../interfaces/task.interface';
 import { ITaskFormModalControls } from '../interfaces/task-form.modal.controls.interface';
 import { generateUniqueIdWithTimestamp } from '../utils/generate-unique-id-with-tomestamp';
@@ -12,19 +12,38 @@ import { IComment } from '../interfaces/comment.interface';
 })
 export class TaskService {
   private todoTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly todoTasks = this.todoTasks$
-    .asObservable()
-    .pipe(map((tasks) => structuredClone(tasks)));
+  readonly todoTasks = this.todoTasks$.asObservable().pipe(
+    map((tasks) => structuredClone(tasks)),
+    tap((tasks) => this.saveTasksToLocalStorage(TaskStatusEnum.TODO, tasks)),
+  );
 
   private doingTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly doingTasks = this.doingTasks$
-    .asObservable()
-    .pipe(map((tasks) => structuredClone(tasks)));
+  readonly doingTasks = this.doingTasks$.asObservable().pipe(
+    map((tasks) => structuredClone(tasks)),
+    tap((tasks) => this.saveTasksToLocalStorage(TaskStatusEnum.DOING, tasks)),
+  );
 
   private doneTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly doneTasks = this.doneTasks$
-    .asObservable()
-    .pipe(map((tasks) => structuredClone(tasks)));
+  readonly doneTasks = this.doneTasks$.asObservable().pipe(
+    map((tasks) => structuredClone(tasks)),
+    tap((tasks) => this.saveTasksToLocalStorage(TaskStatusEnum.DONE, tasks)),
+  );
+
+  constructor() {
+    const todoTasksFromStorage = this.getTasksFromLocalStorage(
+      TaskStatusEnum.TODO,
+    );
+    const doingTasksFromStorage = this.getTasksFromLocalStorage(
+      TaskStatusEnum.DOING,
+    );
+    const doneTasksFromStorage = this.getTasksFromLocalStorage(
+      TaskStatusEnum.DONE,
+    );
+
+    this.todoTasks$.next(todoTasksFromStorage);
+    this.doingTasks$.next(doingTasksFromStorage);
+    this.doneTasks$.next(doneTasksFromStorage);
+  }
 
   addTask(taskInfos: ITaskFormModalControls) {
     const newTask: ITask = {
@@ -102,5 +121,14 @@ export class TaskService {
     };
 
     return taskListObj[taskStatus];
+  }
+
+  private saveTasksToLocalStorage(key: string, tasks: ITask[]) {
+    localStorage.setItem(key, JSON.stringify(tasks));
+  }
+
+  private getTasksFromLocalStorage(key: string): ITask[] {
+    const tasksJSON = localStorage.getItem(key);
+    return tasksJSON ? JSON.parse(tasksJSON) : [];
   }
 }
